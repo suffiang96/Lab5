b85db708ed8bc38935e90c3d2c1d31cf528f8375
suffiang96 <56616975+suffiang96@users.noreply.github.com>
2019-12-10 20:41:18 -0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 var center = [48.009296, -124.151837];
 
 var chinmap = new L.Map('mapone', {
-	layers: [chintiles][sprglayer],
-	layers: [smrlayer][falllayer],
+	layers: [chintiles][chintiles2],
 	center: center,
 	zoom: 9
 });
@@ -12,63 +11,66 @@ var chintiles =	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.pn
 				id: 'mapbox.light',
 				accessToken: 'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazN2MjZ3a2QwN3A1M2Vvd3BoZWY2aGptIn0.TshYd57hSztXfIni3EbVkg'
 			}).addTo(chinmap);
-
-
-chinmap.attributionControl.setPrefix('');
+var chintiles2 = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+				id: 'mapbox.outdoors',
+				accessToken: 'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazN2MjZ3a2QwN3A1M2Vvd3BoZWY2aGptIn0.TshYd57hSztXfIni3EbVkg'
+			}).addTo(chinmap);
 
 var falllayer =  $.getJSON("fallchnook.json",function(fallData){
 		    L.geoJson(fallData, {
 					style: function (feature){
-					   return { color: 'red',
-					          dashArray: '3',
-					          weight: 2 ,
-					          fillOpacity: 0.7 };
+					   return { color: '#cc7c27',
+					          weight: 2.5
+									};
 					 },
-					 onEachFeature: function( feature, layer ){
+			  	 onEachFeature: function( feature, layer ){
 					   layer.bindPopup(  "This waterway hosts Chinook salmon in the Fall" )
 					 }
 				 }).addTo(chinmap);
-					});
+			 });
 
-var smrlayer =	$.getJSON("smrchnook.json",function(smrData){
+let smrlayer =
+L.layerGroup().addTo( chinmap )
+$.getJSON("smrchnook.json",function(smrData){
 		    L.geoJson(smrData, {
-					style: function (feature){
-					   return { color: 'yellow',
-					          dashArray: '6',
-					          weight: 2 ,
-					          fillOpacity: 0.7 };
-					 },
-					 onEachFeature: function( feature, layer ){
-					   layer.bindPopup(  "This waterway hosts Chinook salmon in the Summer" )
-					 }
-				 }).addTo(chinmap);
-					});
+				  onEachFeature: addMyData,
+   })
+ })
+ function addMyData( feature, layer ){
+   smrlayer.addLayer( layer )
+ };
 
-	var sprglayer =	$.getJSON("sprchnook.json",function(sprgData){
-		L.geoJson(sprgData, {
-					style: function (feature){
-					   return { color: 'green',
-					          dashArray: '6',
-					          weight: 2,
-					          fillOpacity: 0.7 };
-					 },
-					 onEachFeature: function( feature, layer ){
-					   layer.bindPopup(  "This waterway hosts Chinook salmon in the Spring" )
-					 }
-				 }).addTo(chinmap);
-					});
+ var osmGeocoder = new L.Control.OSMGeocoder();
+ chinmap.addControl(osmGeocoder);
+
+
+var baseMaps ={
+	"Light": chintiles,
+	"Topographic" :chintiles2
+};
+var layerControl = {
+	"Summer": smrlayer
+}
+L.control.layers(baseMaps,layerControl).addTo(chinmap);
 
 
 
+					L.control.legend({
+				    items: [
+				        {color: '#cc7c27', label: 'Fall Chinook Run'},
+								{color: '#0099ff', label: 'Summer Chinook Run'},
+				    ],
+				    collapsed: true,
 
-
+				    buttonHtml: 'show legend'
+				}).addTo(chinmap);
 
 
  var distmap = new L.Map('maptwo', {
 	 layers: [disttiles][distlayer],
 	 center: center,
 	 zoom: 9,
-	 zoomControl: false
  });
 
  var disttiles = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -76,35 +78,61 @@ var smrlayer =	$.getJSON("smrchnook.json",function(smrData){
 	  id: 'mapbox.light',
 	  accessToken: 'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazN2MjZ3a2QwN3A1M2Vvd3BoZWY2aGptIn0.TshYd57hSztXfIni3EbVkg',
 	 }).addTo(distmap);
+	 var disttiles2 = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	 				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	 				id: 'mapbox.outdoors',
+	 				accessToken: 'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazN2MjZ3a2QwN3A1M2Vvd3BoZWY2aGptIn0.TshYd57hSztXfIni3EbVkg'
+	 			}).addTo(chinmap);
 
-var damslayer=	 $.getJSON("dams.json",function(damData){
-		 L.geoJson(damData, {
-			 style: function(feature){
-				 return { color: "blue",
-									};
-			 },
-			 onEachFeature: function( feature, layer ){
-				 layer.bindPopup( "<p>This dam is on the " + feature.properties.STREAMNAME + " and is managed by the " + feature.properties.AGENCY + "</p>" + "<p>Hatchery: " + feature.properties.HATCHNAME + "</p><p>Fish type: " + feature.properties.FISHTYPE + "</p>")
-			 }
-		 }).addTo(distmap);
-			});
+
 
 var distlayer = 	$.getJSON("anadromous.json",function(anadData){
-				L.geoJson(anadData, {
-					style: function(feature){
-					   return { color: 'blue',
-					          dashArray: '3',
-					          weight: 2 ,
-					          fillOpacity: 0.7 };
-					 },
-					 onEachFeature: function( feature, layer ){
-					   layer.bindPopup(  "<b>Waterway name:</b> " + feature.properties.STRMNAME )
-					 }
-				 }).addTo(distmap);
-					});
+	L.geoJson(anadData, {
+	style: function(feature){
+	 return { color: '#bf3939',
+		 weight: 1.5
+		};
+	 },
+ onEachFeature: function( feature, layer ){
+	 layer.bindPopup(  "<b>Waterway name:</b> " + feature.properties.STRMNAME )
+}
+		}).addTo(distmap);
+								});
 
-					// chinmap.sync(distmap);
-					// distmap.sync(chinmap);
-					//
-					mapone.sync(maptwo);
-					maptwo.sync(mapone);
+var geojsonMarkerOptions = {
+						 	     radius: 4,
+						 	     fillColor: "#325aa8",
+						 	     color: "#000",
+						 	     weight: 1,
+						 	     opacity: 1,
+						 	     fillOpacity: 0.8
+						 	 };
+
+var damslayer=	 $.getJSON("dams.json",function(damData){
+	 L.geoJson(damData, {
+	 pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+	 },
+	 onEachFeature: function( feature, layer ){
+		layer.bindPopup( "<p>This dam is on the " + feature.properties.STREAMNAME + " and is managed by the " + feature.properties.AGENCY + "</p>" + "<p>Hatchery: " + feature.properties.HATCHNAME + "</p><p>Fish type: " + feature.properties.FISHTYPE + "</p>")
+	 }
+}).addTo(distmap);
+		});
+
+var osmGeocoder = new L.Control.OSMGeocoder();
+distmap.addControl(osmGeocoder);
+
+var baseMaps ={
+	"Light": disttiles,
+	"Topographic" :disttiles2
+};
+L.control.layers(baseMaps).addTo(distmap);
+
+L.control.legend({
+ items: [
+	 {color: '#bf3939', label: 'Stream with anadromous fish present'},
+	 {color: '#325aa8', label: 'Dams'},
+	       ],
+ collapsed: true,
+  buttonHtml: 'show legend'
+				}).addTo(distmap);
